57c89d771e180e54251f4d5fd08c659a12f92d88
Timofey <timoxa17@gmail.com>
2023-01-29 20:58:22 +0300

    package updates

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-	plugins: [createHtmlPlugin()],
-}));
