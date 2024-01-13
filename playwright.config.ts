3ad418f3f0d2aebccb2d5df40eaf0f333b2c1aab
Timofey <timoxa17@gmail.com>
2024-01-14 01:29:27 +0300

    playwright added

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	testDir: './tests',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	reporter: 'html',
+	use: {
+		trace: 'on-first-retry',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] },
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] },
+		},
+	],
+	webServer: {
+		command: 'npm run dev -- --port=5174',
+		url: 'http://localhost:5174/',
+		reuseExistingServer: !process.env.CI,
+	},
+});
